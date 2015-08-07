a148667c87fcd50a8a9f57153183b46b2d805e47
vsrinivasan <srinivasan.v39@gmail.com>
2015-08-07 23:36:03 +0530

    ThirdCommit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  def hello
+  render text: "This is the first Ruby on rails application by V.Srinivasan
+  also saved in github"
+  end
 end
